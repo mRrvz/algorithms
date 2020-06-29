d35fc3ee35e3f08f30bc8a8c69bad8c1b456c71d
Romanov Alexey <romanov.alexey2000@gmail.com>
2020-06-29 15:07:11 +0300

    UPD: qsort

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -3,15 +3,15 @@ from utils import check_correctness
 
 
 def partition(arr, p, r):
-    pivot = arr[r - 1]
+    pivot = arr[r]
     i = p - 1
 
     for j in range(p, r):
-        if arr[j] < pivot:
+        if arr[j] <= pivot:
             i = i + 1
             arr[j], arr[i] = arr[i], arr[j]
     
-    arr[i + 1], arr[r - 1] = arr[r - 1], arr[i + 1]
+    arr[i + 1], arr[r] = arr[r], arr[i + 1]
     return i + 1
 
 
@@ -22,15 +22,18 @@ def quick_sort(arr):
             __quick_sort(arr, p, q - 1)
             __quick_sort(arr, q + 1, r)
 
-    __quick_sort(arr, 0, len(arr))
+    __quick_sort(arr, 0, len(arr) - 1)
 
 
 
 def main():
-    arr = [randint(-10, 10) for i in range(10)]
-    print(arr)
+    print(arr := [randint(-10, 10) for i in range(10)])
     quick_sort(arr)
-    print(arr)
+    
+    if check_correctness(arr):
+        print("QSORT IS OK")
+    else:
+        print("QSORT FAIL")
 
 
 if __name__ == "__main__":
